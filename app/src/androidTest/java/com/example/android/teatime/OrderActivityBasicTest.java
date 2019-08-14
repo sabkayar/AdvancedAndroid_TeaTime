48c271177fb68a305fb57518f4c5e82e768e8e7b
Praveen <pkchmits@gmail.com>
2019-08-14 15:56:42 +0530

    Basic UI Test Completed

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -1,36 +1,74 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.teatime;
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * This test demos a user clicking the decrement button and verifying that it properly decrease
  * the quantity the total cost.
  */
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+// DONE (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    // DONE (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<OrderActivity> mOrderActivityActivityTestRule =
+            new ActivityTestRule<>(OrderActivity.class);
 
-    // TODO (3) Finish writing this test which will:
-    //          - Check that the initial quantity is zero
-    //          - Click on the decrement button
-    //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
-
+    @Test
     public void clickDecrementButton_ChangesQuantityAndCost() {
+        //1. Find the view
+
+        //2. Perform the action on the view
+        // Both 1 and 2
+
+
+        //3. Check if view does what you expected
+
+
+
+        // DONE (3) Finish writing this test which will:
+        //          - Check that the initial quantity is zero
+        //          - Click on the decrement button
+        //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+
+
+        //          - Check that the initial quantity
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+        //          - Click on the decrement but
+        onView(withId(R.id.decrement_button)).perform(click());
+        //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
     }
+
 }
